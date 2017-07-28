1aa8725e346fdc66a220a4869c62946fe2e05859
Paula <paulasalgado3@gmail.com>
2017-07-28 14:38:55 -0300

    con posicion del relay

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var mqtt = require('mqtt');  
+var express = require("express");
+var bodyParser = require("body-parser");
 
 var client  = mqtt.connect("mqtt://localhost",{
   protocolId: 'MQIsdp',
@@ -7,30 +9,48 @@ var client  = mqtt.connect("mqtt://localhost",{
   password: "password3"
 });
 
-
+var app = express();
+app.use(bodyParser.json());
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
 
 //Dispositivos
 var dispositivos = new Array();
 var dispositivo1 = new Object();
     dispositivo1.id = "39e0ie";
+    dispositivo1.posicion = "1";
     dispositivo1.tipo = "outlet";
-    dispositivo1.estado = 0;
+    dispositivo1.estado = "false";
     dispositivo1.ubicacion = "comedor";
-    dispositivo1.editar = "";
 var dispositivo2 = new Object();
-    dispositivo2.id = "4rfj3l4";
+    dispositivo2.id = "39e0ie";
+    dispositivo2.posicion = "2";
     dispositivo2.tipo = "outlet";
-    dispositivo2.estado = 1;
+    dispositivo2.estado = "true";
     dispositivo2.ubicacion = "pieza1";
-    dispositivo2.editar="";
+var dispositivo3 = new Object();
+    dispositivo3.id = "mddid9e";
+    dispositivo3.posicion = "1";
+    dispositivo3.tipo = "outlet";
+    dispositivo3.estado = "true";
+    dispositivo3.ubicacion = "jardin";
+var dispositivo4 = new Object();
+    dispositivo4.id = "md4id9e";
+    dispositivo4.posicion = "1";
+    dispositivo4.tipo = "outlet";
+    dispositivo4.estado = "true";
+    dispositivo4.ubicacion = "jardin2";
     dispositivos.push(dispositivo2);
     dispositivos.push(dispositivo1);
+    dispositivos.push(dispositivo4);
+    dispositivos.push(dispositivo3);
 
 //Metodos
-function existe_dispositivo(id){
+function existe_dispositivo(id,posicion){
 	var existe = false;
 	for (i = 0; i < dispositivos.length; i++) {
-            if(dispositivos[i].id==id){
+            if(dispositivos[i].id==id && dispositivos[i].posicion == posicion){
 			existe = true;
             }
         }
@@ -41,10 +61,18 @@ function crear_dispositivo(dispositivo){
 }
 function borrar_dispositivo(dispositivo){
 	for (i = 0; i < dispositivos.length; i++) {
-            if(dispositivos[i].id==dispositivo.id){
+            if(dispositivos[i].id==dispositivo.id && dispositivos[i].posicion == dispositivo.posicion){
                         dispositivos.splice(i,1);
             }
         }
+}
+function modificar_dispositivo(dispositivo){
+	for (i = 0; i < dispositivos.length; i++){
+		if(dispositivos[i].id==dispositivo.id && dispositivos[i].posicion == dispositivo.posicion){
+			dispositivos[i]=dispositivo;
+		}
+	}
+
 }
 /*
 home/feature/outlet/new
@@ -53,21 +81,36 @@ home/feature/outlet/delete
 
 client.subscribe ('home/feature/outlet/new', {qos:1});
 client.subscribe ('home/feature/outlet/delete', {qos:1});
+client.subscribe ('home/feature/outlet/change_state', {qos:1});
 client.on('message', function(topic,message){
 	if(topic.toString() == "home/feature/outlet/new"){
 		var dispositivo = JSON.parse(message.toString()); 
-		if(!existe_dispositivo(dispositivo.id)){
+		if(!existe_dispositivo(dispositivo.id, dispositivo.posicion)){
 			crear_dispositivo(dispositivo);
-			console.log("Nuevo Dispositivo Id: " + dispositivo.id);  
+			console.log("Nuevo Dispositivo Id: " + dispositivo.id + ":"+ dispositivo.posicion );  
 		}	
 	}else if (topic.toString() == "home/feature/outlet/delete"){
 		var dispositivo = JSON.parse(message.toString());
 		if(existe_dispositivo(dispositivo.id)){
 			borrar_dispositivo(dispositivo);
-			console.log("Dispositivo Borrado Id: " + dispositivo.id);
+			console.log("Dispositivo Borrado Id: " + dispositivo.id+":"+dispositivo.posicion);
 		}
+	}else if(topic.toString() == "home/feature/outlet/change_state"){
+		var dispositivo = JSON.parse(message.toString());
+		modificar_dispositivo(dispositivo);
+		console.log("Dispositivo Modificado Id: " + dispositivo.id+":"+dispositivo.posicion);
 	}
 });
 
 
+app.get(/^(.+)$/, function(req, res){ 
+    switch(req.params[0]) {
+	case '/dispositivos':
+		var jdispositivos = JSON.stringify(dispositivos);
+		res.send(jdispositivos);
+		res.end();
+	    break;
 
+    	default: res.sendFile( __dirname + req.params[0]); 
+    }
+ });
